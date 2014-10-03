ee2b0d93f42cbe1099453d4845e8474b64e075fa
Anton <mblxac@gmail.com>
2014-10-03 16:39:19 +0300

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -4,11 +4,18 @@ return array(
         //Mail factory
         'factories' => array(
             'MailHtmlGenerator' => function ($sm) {
+                $config = $sm->get('Config');
                 $mail = new \EmailHtmlGenerator\Service\MailHtmlGenerator();
                 $transport = new \Zend\Mail\Transport\Sendmail();
                 
                 $mail->setTransport($transport);
-                $mail->setViewRenderer($sm->get('ViewRenderer'));
+                $viewRenderer = new \Zend\View\Renderer\PhpRenderer();
+                $resolver = new \Zend\View\Resolver\AggregateResolver();
+                $viewRenderer->setResolver($resolver);
+                $viewsMap = new \Zend\View\Resolver\TemplatePathStack();
+                $viewsMap->addPaths($config['view_manager']['emails_path']);
+                $resolver->attach($viewsMap);
+                $mail->setViewRenderer($viewRenderer);
                 return $mail;
             },
         ),
